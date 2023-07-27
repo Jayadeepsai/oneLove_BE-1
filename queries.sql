ef5d2da99e5b11308f9eb1f76355886efe689f22
Srikanth Pusapati <spusapati@Srikanths-MBP.lan>
2023-07-27 00:22:05 -0500

    updated db table

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `onelove`.`vaccination` (             --creation of vaccination table  
   `vaccination_id` INT NOT NULL AUTO_INCREMENT,
-  `vaccineName` VARCHAR(150) NULL,
+  `vaccine_name` VARCHAR(150) NULL,
   `date` VARCHAR(45) NULL,
   `dosage` VARCHAR(45) NULL,
   `cost` VARCHAR(45) NULL,
@@ -9,9 +9,9 @@ CREATE TABLE `onelove`.`vaccination` (             --creation of vaccination tab
 
   CREATE TABLE `onelove`.`items` (                 --creation of items table 
   `item_id` INT NOT NULL AUTO_INCREMENT,
-  `itemType` VARCHAR(45) NULL,
-  `itemName` VARCHAR(45) NULL,
-  `itemPrice` VARCHAR(45) NULL,
-  `itemDescription` VARCHAR(199) NULL,
-  `itemImage` VARCHAR(1000) NULL,
+  `item_type` VARCHAR(45) NULL,
+  `item_name` VARCHAR(45) NULL,
+  `item_price` VARCHAR(45) NULL,
+  `item_description` VARCHAR(199) NULL,
+  `item_image` VARCHAR(1000) NULL,
   PRIMARY KEY (`item_id`));
