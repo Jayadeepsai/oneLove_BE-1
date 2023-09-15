2f579d665c5ba579e0b3b079b19f0aa09d754780
Jayadeepsai <jayadeepsaim@gmail.com>
2023-09-15 17:30:18 +0530

    changes in ratings

--- src/ratings_reviews/ratings.js
+++ src/ratings_reviews/ratings.js
@@ -9,25 +9,84 @@ const db = require('../../dbConnection');
 ratings.use(express.json()); // To parse JSON bodies
 ratings.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies
 
-ratings.post('/rating-review',async(req,res)=>{
-    try{
-        const {user_id, ratings_reviews } = req.body;
-        const sql = 'INSERT INTO onelove.rating_review(user_id, ratings_reviews) VALUES(?, ?)';
-        const values = [user_id, JSON.stringify(ratings_reviews)]
+// ratings.post('/rating-review',async(req,res)=>{
+//     try{
+//         const {user_id, ratings_reviews } = req.body;
+//         const sql = 'INSERT INTO onelove.rating_review(user_id, ratings_reviews) VALUES(?, ?)';
+//         const values = [user_id, JSON.stringify(ratings_reviews)]
 
-        const [result] = await db.query(sql,values);
+//         const [result] = await db.query(sql,values);
+//         res.status(200).json({
+//             data: result,
+//             message: "Data posted"
+//         });
+
+//     }catch(err){
+//         console.error('Error posting data:', err.message);
+//         res.status(400).json({
+//             message: err
+//         });
+//     }
+// });
+
+
+ratings.post('/rating-review', async (req, res) => {
+    try {
+        const { user_id, ratings_reviews } = req.body;
+
+        // Check if a row with the specified user_id exists
+        const checkIfExistsQuery = 'SELECT ratings_reviews FROM onelove.rating_review WHERE user_id = ?';
+        const [existingRows] = await db.query(checkIfExistsQuery, [user_id]);
+
+        let updatedReviews = [];
+
+        if (existingRows.length > 0) {
+            // If a row exists, fetch the existing JSON array
+            const existingReviews = existingRows[0].ratings_reviews;
+            
+            if (Array.isArray(existingReviews)) {
+                // If it's an array, concatenate the new data
+                updatedReviews = existingReviews.concat(ratings_reviews);
+            } else {
+                try {
+                    // If it's not an array, parse it and then concatenate
+                    const parsedReviews = JSON.parse(existingReviews);
+                    updatedReviews = parsedReviews.concat(ratings_reviews);
+                } catch (parseError) {
+                    // Handle JSON parsing error here
+                    console.error('Error parsing existingReviews:', parseError);
+                    res.status(400).json({
+                        message: 'Error parsing existingReviews',
+                    });
+                    return;
+                }
+            }
+            
+            // Update the JSON data in the database
+            const updateQuery = 'UPDATE onelove.rating_review SET ratings_reviews = ? WHERE user_id = ?';
+            const updateValues = [JSON.stringify(updatedReviews), user_id];
+            await db.query(updateQuery, updateValues);
+        } else {
+            // If no row exists, insert a new row with the new data
+            const insertQuery = 'INSERT INTO onelove.rating_review (user_id, ratings_reviews) VALUES (?, ?)';
+            const insertValues = [user_id, JSON.stringify(ratings_reviews)];
+            await db.query(insertQuery, insertValues);
+        }
+
         res.status(200).json({
-            data: result,
-            message: "Data posted"
+            message: "Data posted or updated"
         });
-
-    }catch(err){
-        console.error('Error posting data:', err.message);
+    } catch (err) {
+        console.error('Error posting/updating data:', err.message);
         res.status(400).json({
             message: err
         });
     }
 });
+
+
+
+
 
 ratings.get('/rating-review',async(req,res)=>{
    try{
