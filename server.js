f368d15bee877b4ac87f6def31d7655eaf7506eb
Jayadeepsai <jayadeepsaim@gmail.com>
2023-09-24 09:26:49 +0530

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app')
 // Create an instance of the http server to handle HTTP requests
-const port =process.env.PORT || 8081;
+const port =process.env.PORT || 3000;
 
 
 const server = http.createServer(app);
