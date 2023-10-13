4bfceb6f9e894548f4d720ca7b0b70b5fe19e0a0
Jayadeepsai <jayadeepsaim@gmail.com>
2023-10-13 10:09:08 +0530

    unable crypto

--- jwtMiddleware.js
+++ jwtMiddleware.js
@@ -1,25 +1,19 @@
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+// const crypto = require('crypto');
+
+require('dotenv').config();
 
 // Generate a random secret key
-const secret_Key = crypto.randomBytes(32).toString('hex');
-console.log(secret_Key);
+// const secret_Key = crypto.randomBytes(32).toString('hex');
+// console.log(secret_Key);
 
-const secretKey = secret_Key;
+const secretKey = process.env.SECRET_KEY_JWT;
 
-
-
-// Generate a JWT token with expiration time
-// function generateToken(userId) {
-//     const expiresIn = 24 * 60 * 60; // 24 hours in seconds
-//     const expirationTime = Math.floor(Date.now() / 1000) + expiresIn; // Current time + 24 hours
-  
-//     return jwt.sign({ userId, exp: expirationTime }, secretKey);
-//   }
 
   function generateToken(userId) {
     return jwt.sign({ userId }, secretKey, { expiresIn: '24h' }); // You can adjust the expiration time
   }
+
 
 
 // Verify a JWT token
@@ -29,7 +23,6 @@ function verifyToken(req, res, next) {
   if (!tokenHeader) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
-
   // Extract the token part
   const token = tokenHeader.split(' ')[1];
 
@@ -42,23 +35,16 @@ function verifyToken(req, res, next) {
       console.error('JWT verification error:', err);
       return res.status(403).json({ message: 'Forbidden' });
     }
-
     req.userId = decoded;
     next();
   });
 }
 
+
+
 function generateRefreshToken(userId) {
     return jwt.sign({ userId }, secretKey, { expiresIn: '7d' }); // Use the same secret key
   }
-
-// Generate a refresh token with an expiration time
-// function generateRefreshToken(userId) {
-//     const expiresIn = 7 * 24 * 60 * 60; // 7 days in seconds
-//     const expirationTime = Math.floor(Date.now() / 1000) + expiresIn; // Current time + 7 days
-  
-//     return jwt.sign({ userId, exp: expirationTime }, secretKey);
-//   }
 
 // Handle token refresh
 function refreshToken(req, res) {
--- src/oneSignal/notifications.js
+++ src/oneSignal/notifications.js
@@ -6,77 +6,13 @@ require('dotenv').config();
 const OneSignal = require('@onesignal/node-onesignal')
 
 
-
-// router.post('/sendMessages', async (req, res, next) => {
-
-//     const externalId = req.body.externalId
-
-//     const messageData = new adminMessage({
-//         title: req.body.title,
-//         description: req.body.description,
-//         to:req.body.to
-//     })
-//     //8fda6cf4-bdbe-4f2e-a709-24f8990ad307
-//     const ONESIGNAL_APP_ID = '8fda6cf4-bdbe-4f2e-a709-24f8990ad307';
-
-//     const app_key_provider = {
-//         getToken() {
-//             //return 'ZjA4ZTMyOGEtOTEzMy00MzQyLTg2MmItYWM3YTExMTM2YzI2';
-//             return 'OWE5OTk1MTctMjM1NC00ZTZiLWFhNTgtMmY2MTlkNTY0NWZm'
-//         }
-//     };
-
-//     const configuration = OneSignal.createConfiguration({
-//         authMethods: {
-//             app_key: {
-//                 tokenProvider: app_key_provider
-//             }
-//         }
-//     });
-//     const client = new OneSignal.DefaultApi(configuration);
-
-//     const notification = new OneSignal.Notification();
-//     notification.app_id = ONESIGNAL_APP_ID;
-//     // notification.included_segments = ['Subscribed Users'];
-//     notification.include_external_user_ids = externalId
-//     console.log(messageData)
-//     notification.headings = {
-//         en: messageData.title
-//     }
-//     notification.contents = {
-//         en: messageData.description
-//     };
-//     const { id } = await client.createNotification(notification);
-
-//     const response = await client.getNotification(ONESIGNAL_APP_ID, id);
-
-//     console.log(response)
-//     const data = await messageData.save()
-//     try {
-//         return res.status(201).json({
-//             message: "Notification sent successfully",
-//             messsageDetails: data,
-//             response
-//         })
-//     } catch (err) {
-//         return res.status(500).json({
-//             message: "Error sending the message"
-//         })
-//     }
-
-
-// })
-
-
-
 async function sendnotification( Name,mess,uniqId) {
     try {
-        const ONESIGNAL_APP_ID = '3fd29a95-0021-4625-83ef-3998793e9c27';
+        const ONESIGNAL_APP_ID = process.env.ONESIGNAL_APP_ID;
  
         const app_key_provider = {
             getToken() {
-            //  return 'MjY5ZDJjZWUtZDUyNi00YjZmLWJmZjQtNGM2ZmYzM2FmZTM2'
-            return 'MDllM2QyN2MtYzJjNC00ZmE5LThkNDUtZjgyY2JkYTNlYjA5';
+            return process.env.REST_API_KEY;
             }
         };
       
