9f5c553c63aee896f427c406ac5ee9fa303206b7
Jayadeepsai <jayadeepsaim@gmail.com>
2023-11-11 13:47:30 +0530

    connection

--- dbConnection.js
+++ dbConnection.js
@@ -1,36 +1,76 @@
+    // const mysql = require('mysql2');
+    // require('dotenv').config();
+    // const connection = mysql.createConnection({
+
+    //     host: process.env.DB_HOST,
+    //     port: process.env.DB_PORT,
+    //     user: process.env.DB_USER,
+    //     password: process.env.DB_PASSWORD,
+    //     database: process.env.DB_DATABASE,
+    //     charset: 'utf8mb4',
+    //     ssl: {
+    //         rejectUnauthorized: true,
+    //       },
+    // })
+
+
+    // connection.connect(function (error) {
+    //     if (error) throw error
+    //     else{
+    //         console.log('connected to mysql database')
+    //     //  var sql = `show tables;`
+        
+        
+
+    //     // connection.query(sql, (err, result) => {
+    //     //     if (err) throw err;
+    //     //     console.log("Qurey Executed",result)
+    //     //  })
+    //     }
+    // })
+
+
+    // module.exports = connection.promise();
+
     const mysql = require('mysql2');
-    require('dotenv').config();
-    const connection = mysql.createConnection({
+require('dotenv').config();
 
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        charset: 'utf8mb4',
-        ssl: {
-            rejectUnauthorized: true,
-          },
-    })
+let connection;
 
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    charset: 'utf8mb4',
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  });
 
-    connection.connect(function (error) {
-        if (error) throw error
-        else{
-            console.log('connected to mysql database')
-        //  var sql = `show tables;`
-        
-        
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      setTimeout(handleDisconnect, 2000); // Attempt to reconnect after 2 seconds
+    } else {
+      console.log('Connected to MySQL database');
+    }
+  });
 
-        // connection.query(sql, (err, result) => {
-        //     if (err) throw err;
-        //     console.log("Qurey Executed",result)
-        //  })
-        }
-    })
+  connection.on('error', (err) => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
 
+handleDisconnect();
 
-    module.exports = connection.promise();
+module.exports = connection.promise();
 
-    
     
