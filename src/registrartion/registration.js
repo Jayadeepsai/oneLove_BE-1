02ae5cbae213ebfa5344eb1efb4813fe2f0c6d99
Jayadeepsai <jayadeepsaim@gmail.com>
2023-11-14 14:49:30 +0530

    login

--- src/registrartion/registration.js
+++ src/registrartion/registration.js
@@ -303,13 +303,47 @@ registration.post('/registration-mobile-number', async (req, res) => {
         }
 
         // First, retrieve the user's data
-        const selectUserSql = `SELECT a.*, c.*, u.*, i.*, s.*, c1.clinic_name AS clinic_name
-                  FROM onelove.users u
-                   LEFT JOIN address a ON u.address_id = a.address_id
-                   LEFT JOIN contact_details c ON u.contact_id = c.contact_id
-                   LEFT JOIN store s ON u.store_id = s.store_id
-                   LEFT JOIN clinics c1 ON u.clinic_id = c1.clinic_id
-                   LEFT JOIN images i ON u.image_id = i.image_id WHERE c.mobile_number=?`;
+        const selectUserSql = ` SELECT
+        u.user_id,
+        u.user_type,
+        u.address_id,
+        u.contact_id,
+        u.user_name,
+        u.store_id,
+        u.service_id,
+        u.clinic_id,
+        u.image_id,
+        u.external_id,
+        a.address,
+        a.city,
+        a.state,
+        a.zip,
+        a.country,
+        a.landmark,
+        a.address_type,
+        a.lat_cords,
+        a.lan_cords,
+        c.mobile_number,
+        c.email,
+        s.store_name,
+        c1.clinic_name,
+        i.image_type,
+        i.image_url,
+        s.food_treats,
+        s.accessories,
+        s.toys,
+        s.health_care,
+        s.dog_service,
+        s.breader_adoption_sale
+    FROM
+        onelove.users u
+        LEFT JOIN onelove.address a ON u.address_id = a.address_id
+        LEFT JOIN onelove.contact_details c ON u.contact_id = c.contact_id
+        LEFT JOIN onelove.store s ON u.store_id = s.store_id
+        LEFT JOIN onelove.clinics c1 ON u.clinic_id = c1.clinic_id
+        LEFT JOIN onelove.images i ON u.image_id = i.image_id
+    WHERE
+        c.mobile_number=?;`
 
         const [userData] = await connection.query(selectUserSql, [mobile_number]);
 
