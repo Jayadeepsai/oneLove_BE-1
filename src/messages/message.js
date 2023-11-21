2e0bf1fef64f80626626fbded1b836d6f5e74b67
oneLovePC <oenlovepc2021@gmail.com>
2023-11-21 14:39:10 +0530

    socket.io

--- src/messages/message.js
+++ src/messages/message.js
@@ -47,6 +47,7 @@ io.on('connection', (socket) => {
       const { sender_id, receiver_id, message } = data;
       // Get the current date and time
       const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+      logger.info("Time:",currentTime);
 
       const sql = 'INSERT INTO messages (sender_id, receiver_id, message, time) VALUES (?, ?, ?, ?)';
 
