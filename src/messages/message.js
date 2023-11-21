958ba04cb89ef29fbb10b28a0e292956963b096a
oneLovePC <oenlovepc2021@gmail.com>
2023-11-21 14:09:31 +0530

    socket.io

--- src/messages/message.js
+++ src/messages/message.js
@@ -6,21 +6,42 @@ const db = require('../../dbConnection');
 const cron = require('node-cron');
 const moment = require('moment');
 const logger = require('../../logger');
+const http = require('http')
+const { Server } = require("socket.io")
+
+
 
 // Include Socket.io and create a socket server instance
-const httpServer = require('http').createServer();
-const io = require('socket.io')(httpServer);
+
+const httpServer = http.createServer();
+// const io = require('socket.io')(httpServer);
+
+const io = new Server(httpServer)
+
+
+
+
+
+
 
 message.use(express.json()); // To parse JSON bodies
 message.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies
 
+const userSocketMap = new Map();
 
 // Initialize the socket server
 io.on('connection', (socket) => {
   logger.info('A user connected');
+  console.log('user connected', socket.id)
+
+
+  socket.on('set_user_id', (user_id) => {
+    userSocketMap.set(user_id, socket.id);
+  });
 
   // Handle new messages here
-  socket.on('newMessage', async (data) => {
+  socket.on('send_message', async (data) => {
+    console.log('data', data)
     try {
       // Get data from the request body
       const { sender_id, receiver_id, message } = data;
@@ -32,13 +53,24 @@ io.on('connection', (socket) => {
       // Insert the message into the database
       await db.query(sql, [sender_id, receiver_id, message, currentTime]);
 
-      // Emit the new message to all connected clients
-      io.emit('message', {
-        sender_id,
-        receiver_id,
-        message,
-        time: currentTime,
-      });
+      // io.emit('receive_message', {
+      //   sender_id,
+      //   receiver_id,
+      //   message,
+      //   time: currentTime,
+      // });
+
+
+      // socket.emit("receive_message", data)
+
+
+      const receiverSocketId = userSocketMap.get(receiver_id);
+
+
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit('receive_message', data);
+      }
+
 
       logger.info('Message saved and sent:', data);
 
@@ -47,46 +79,49 @@ io.on('connection', (socket) => {
     }
   });
 
+
+
   socket.on('disconnect', () => {
     logger.warn('A user disconnected');
+    console.log('user disconnected', socket.id)
   });
 });
 
 
-  // Route to get messages between sender_id and receiver_id
+// Route to get messages between sender_id and receiver_id
 message.get('/messages', async (req, res) => {
-    try {
-      // Get sender_id and receiver_id from the URL parameters
-      const { sender_id, receiver_id } = req.query;
-      const sql ='SELECT * FROM messages WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?) ORDER BY time ASC';
-  
-      // Retrieve messages ordered by time (most recent first)
-      const [messages] = await db.query(sql,[sender_id, receiver_id, receiver_id, sender_id]);
-      
-   // Format the timestamps in the response
-   const convo = messages.map((message) => ({
-    ...message,
-    time: moment(message.time).format('YYYY-MM-DD HH:mm:ss'), // Format it as per your database format
-  }));
+  try {
+    // Get sender_id and receiver_id from the URL parameters
+    const { sender_id, receiver_id } = req.query;
+    const sql = 'SELECT * FROM messages WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?) ORDER BY time ASC';
 
-  const chat =JSON.parse(JSON.stringify(convo));
+    // Retrieve messages ordered by time (most recent first)
+    const [messages] = await db.query(sql, [sender_id, receiver_id, receiver_id, sender_id]);
 
-      res.status(200).json({
-        chat,
-        message: constant.SUCCESS_MESSAGE
-      });
-    } catch (error) {
-      logger.error('Error getting messages:', error);
-      res.status(500).json({ error: constant.FAILURE_MESSAGE });
-    }
-  });
+    // Format the timestamps in the response
+    const convo = messages.map((message) => ({
+      ...message,
+      time: moment(message.time).format('YYYY-MM-DD HH:mm:ss'), // Format it as per your database format
+    }));
+
+    const chat = JSON.parse(JSON.stringify(convo));
+
+    res.status(200).json({
+      chat,
+      message: constant.SUCCESS_MESSAGE
+    });
+  } catch (error) {
+    logger.error('Error getting messages:', error);
+    res.status(500).json({ error: constant.FAILURE_MESSAGE });
+  }
+});
 
 
-  message.get('/chat_history', async (req, res) => {
-    try {
-      const { user_id } = req.query;
+message.get('/chat_history', async (req, res) => {
+  try {
+    const { user_id } = req.query;
 
-      const sql = `
+    const sql = `
       SELECT
           u.user_id,
           u.user_name,
@@ -105,73 +140,73 @@ message.get('/messages', async (req, res) => {
       ORDER BY
           latest_time DESC
   `;
-  
-      // Retrieve chat history for the user with the latest time for each user
-      const [chatHistory] = await db.query(sql,[user_id, user_id, user_id]);
-      const Data = JSON.parse(JSON.stringify(chatHistory));
-  
-      res.status(200).json({
-        Data,
-        message: constant.SUCCESS_MESSAGE,
+
+    // Retrieve chat history for the user with the latest time for each user
+    const [chatHistory] = await db.query(sql, [user_id, user_id, user_id]);
+    const Data = JSON.parse(JSON.stringify(chatHistory));
+
+    res.status(200).json({
+      Data,
+      message: constant.SUCCESS_MESSAGE,
     });
-    } catch (error) {
-      logger.error('Error getting chat history:', error);
-      res.status(500).json({ error: constant.FAILURE_MESSAGE });
+  } catch (error) {
+    logger.error('Error getting chat history:', error);
+    res.status(500).json({ error: constant.FAILURE_MESSAGE });
+  }
+});
+
+
+
+message.delete('/delete_old_conversations', async (req, res) => {
+  try {
+    // Calculate the timestamp representing 24 hours ago
+    const currentTime = new Date();
+    const twentyFourHoursAgo = new Date(currentTime - 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+
+    const sql = 'DELETE FROM messages WHERE time < ?';
+
+    // Delete conversations older than 24 hours
+    await db.query(sql, [twentyFourHoursAgo]);
+
+    res.status(200).json({ message: 'Old conversations deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting old conversations:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+const deleteOldConversations = async () => {
+  try {
+    // Calculate the timestamp representing 24 hours ago
+    const currentTime = new Date();
+    const twentyFourHoursAgo = new Date(currentTime - 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+
+    const sql = 'DELETE FROM messages WHERE time < ?';
+
+    // Delete conversations older than 24 hours
+    const [sqlResult] = await db.query(sql, [twentyFourHoursAgo]);
+
+    if (sqlResult.affectedRows > 0) {
+      logger.info('Old conversations deleted');
     }
-  });
 
+  } catch (error) {
+    logger.error('Error deleting old conversations:', error);
+  }
+};
 
-
-  // message.delete('/delete_old_conversations', async (req, res) => {
-  //   try {
-  //     // Calculate the timestamp representing 24 hours ago
-  //     const currentTime = new Date();
-  //     const twentyFourHoursAgo = new Date(currentTime - 24 * 60 * 60 * 1000); // 24 hours in milliseconds
-  
-  //     const sql = 'DELETE FROM messages WHERE time < ?';
-  
-  //     // Delete conversations older than 24 hours
-  //     await db.query(sql, [twentyFourHoursAgo]);
-  
-  //     res.status(200).json({ message: 'Old conversations deleted successfully' });
-  //   } catch (error) {
-  //     console.error('Error deleting old conversations:', error);
-  //     res.status(500).json({ error: 'Internal Server Error' });
-  //   }
-  // });
-  
-  // const deleteOldConversations = async () => {
-  //   try {
-  //     // Calculate the timestamp representing 24 hours ago
-  //     const currentTime = new Date();
-  //     const twentyFourHoursAgo = new Date(currentTime - 24 * 60 * 60 * 1000); // 24 hours in milliseconds
-  
-  //     const sql = 'DELETE FROM messages WHERE time < ?';
-  
-  //     // Delete conversations older than 24 hours
-  //    const [sqlResult] = await db.query(sql, [twentyFourHoursAgo]);
-
-  //    if(sqlResult.affectedRows > 0){
-  //      logger.info('Old conversations deleted');
-  //    }
-      
-  //   } catch (error) {
-  //     logger.error('Error deleting old conversations:', error);
-  //   }
-  // };
-  
-  // Schedule the deletion job to run every 10 mins
-  // cron.schedule('*/10 * * * *', deleteOldConversations); // This schedules the job to run at the beginning of every 10 mins
+//Schedule the deletion job to run every 10 mins
+cron.schedule('*/10 * * * *', deleteOldConversations); // This schedules the job to run at the beginning of every 10 mins
 
 
 
 
 
 
-  // Create a route to get the mobile number based on user_id
+// Create a route to get the mobile number based on user_id
 
-  
-  message.get('/get-mobile-number', async (req, res) => {
+
+message.get('/get-mobile-number', async (req, res) => {
   try {
     const userId = req.query.user_id;
 
