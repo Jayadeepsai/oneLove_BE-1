544527a4487e599f6f9f799fec716a0496594f10
oneLovePC <oenlovepc2021@gmail.com>
2023-12-13 15:19:02 +0530

    clinic

--- src/clinic/clinicAdd.js
+++ src/clinic/clinicAdd.js
@@ -1,205 +1,205 @@
-// const express = require('express');
-// const clinic = express.Router();
-// const bodyParser = require('body-parser');
-// const messages = require('../messages/constants');
+const express = require('express');
+const clinic = express.Router();
+const bodyParser = require('body-parser');
+const messages = require('../messages/constants');
 
-// const db = require('../../dbConnection');
-// const jwtMiddleware = require('../../jwtMiddleware');
-// const logger = require('../../logger');
+const db = require('../../dbConnection');
+const jwtMiddleware = require('../../jwtMiddleware');
+const logger = require('../../logger');
 
-// clinic.use(express.json());
-// clinic.use(express.urlencoded({ extended: true })); 
-
-
-// async function clinicAddQueries(req,res){
-
-// try{
-//     await db.beginTransaction();
-
-//         const {clinic_name, specialisation, clinic_license, experience, education, week_start_day, week_end_day, start_time, end_time} = req.body;
-//         const clinicQuery ='INSERT INTO onelove.clinics(clinic_name, specialisation, clinic_license, experience, education, week_start_day, week_end_day, start_time, end_time) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-//         const clinicValues = [clinic_name, JSON.stringify(specialisation), clinic_license, experience, education, week_start_day, week_end_day, start_time, end_time]
-
-//         await db.query(clinicQuery,clinicValues)
-
-//         await db.commit();
-//         logger.info('Clinic added successfully'); 
-//         res.status(200).json({ message: messages.POST_SUCCESS});
-
-// }catch(err){
-//     await db.rollback();
-//     logger.error('Error in transaction:', err.message);
-//     res.status(400).json({ message:messages.POST_FAILED});
-// }
-// };
-
-// clinic.post('/clinic',jwtMiddleware.verifyToken, (req, res) => {
-//     clinicAddQueries(req, res)
-//         .then(() => {
-//             logger.info('Transaction completed successfully');
-//         })
-//         .catch((err) => {
-//             logger.error('Error in posting clinic data:', err);
-//         });
-// });
+clinic.use(express.json());
+clinic.use(express.urlencoded({ extended: true })); 
 
 
+async function clinicAddQueries(req,res){
 
-// clinic.get('/clinic',jwtMiddleware.verifyToken,async(req,res)=>{
+try{
+    await db.beginTransaction();
 
-//     const { userType } = req;
-//     if (userType !== 'pet_owner') {
-//         return res.status(403).json({ message: messages.FORBID });
-//     }
+        const {clinic_name, specialisation, clinic_license, experience, education, week_start_day, week_end_day, start_time, end_time} = req.body;
+        const clinicQuery ='INSERT INTO onelove.clinics(clinic_name, specialisation, clinic_license, experience, education, week_start_day, week_end_day, start_time, end_time) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        const clinicValues = [clinic_name, JSON.stringify(specialisation), clinic_license, experience, education, week_start_day, week_end_day, start_time, end_time]
 
-//     const sql = `
-//     SELECT  s.*,u.*,a.*,c.*,i.*
-//     FROM users u
-//     LEFT JOIN address a ON u.address_id = a.address_id
-//     LEFT JOIN contact_details c ON u.contact_id = c.contact_id
-//     LEFT JOIN clinics s ON u.clinic_id = s.clinic_id
-//     LEFT JOIN images i ON u.image_id = i.image_id
-//     WHERE u.user_type = 'pet_doctor'`;
+        await db.query(clinicQuery,clinicValues)
 
-//     try{
-//     const [results] = await db.query(sql);
-//         const clinicsData = JSON.parse(JSON.stringify(results));
-//         res.status(200).json({
-//             clinicsData,
-//             message: messages.SUCCESS_MESSAGE
-//         });
+        await db.commit();
+        logger.info('Clinic added successfully'); 
+        res.status(200).json({ message: messages.POST_SUCCESS});
+
+}catch(err){
+    await db.rollback();
+    logger.error('Error in transaction:', err.message);
+    res.status(400).json({ message:messages.POST_FAILED});
+}
+};
+
+clinic.post('/clinic',jwtMiddleware.verifyToken, (req, res) => {
+    clinicAddQueries(req, res)
+        .then(() => {
+            logger.info('Transaction completed successfully');
+        })
+        .catch((err) => {
+            logger.error('Error in posting clinic data:', err);
+        });
+});
+
+
+
+clinic.get('/clinic',jwtMiddleware.verifyToken,async(req,res)=>{
+
+    const { userType } = req;
+    if (userType !== 'pet_owner') {
+        return res.status(403).json({ message: messages.FORBID });
+    }
+
+    const sql = `
+    SELECT  s.*,u.*,a.*,c.*,i.*
+    FROM users u
+    LEFT JOIN address a ON u.address_id = a.address_id
+    LEFT JOIN contact_details c ON u.contact_id = c.contact_id
+    LEFT JOIN clinics s ON u.clinic_id = s.clinic_id
+    LEFT JOIN images i ON u.image_id = i.image_id
+    WHERE u.user_type = 'pet_doctor'`;
+
+    try{
+    const [results] = await db.query(sql);
+        const clinicsData = JSON.parse(JSON.stringify(results));
+        res.status(200).json({
+            clinicsData,
+            message: messages.SUCCESS_MESSAGE
+        });
         
-//     }catch(err){
-//         logger.error('Error fetching data:', err);
-//         res.status(400).json({
-//             message: messages.FAILURE_MESSAGE,
-//         });
-//     }
-// });
+    }catch(err){
+        logger.error('Error fetching data:', err);
+        res.status(400).json({
+            message: messages.FAILURE_MESSAGE,
+        });
+    }
+});
 
 
-// clinic.get('/clinic-user-id',jwtMiddleware.verifyToken, async(req,res)=>{
+clinic.get('/clinic-user-id',jwtMiddleware.verifyToken, async(req,res)=>{
 
-//     const { userType } = req;
-//     if (userType !== 'pet_owner'&& userType !== 'pet_doctor') {
-//         return res.status(403).json({ message: messages.FORBID });
-//     }
+    const { userType } = req;
+    if (userType !== 'pet_owner'&& userType !== 'pet_doctor') {
+        return res.status(403).json({ message: messages.FORBID });
+    }
 
-//     const userId = req.query.user_id; 
+    const userId = req.query.user_id; 
     
-//     if (!userId) {
-//         return res.status(400).json({
-//             message: messages.INVALID_ID,
-//         });
-//     }
+    if (!userId) {
+        return res.status(400).json({
+            message: messages.INVALID_ID,
+        });
+    }
 
-//     const sql = `
-//     SELECT  s.*,u.*,a.*,c.*,i.*
-//     FROM users u
-//     LEFT JOIN address a ON u.address_id = a.address_id
-//     LEFT JOIN contact_details c ON u.contact_id = c.contact_id
-//     LEFT JOIN clinics s ON u.clinic_id = s.clinic_id
-//     LEFT JOIN images i ON u.image_id = i.image_id
-//     WHERE u.user_id = ? AND u.user_type = 'pet_doctor'`;
-//     try{
-//         const [results] = await db.query(sql, [userId]);
-//         const clinicData = JSON.parse(JSON.stringify(results));
+    const sql = `
+    SELECT  s.*,u.*,a.*,c.*,i.*
+    FROM users u
+    LEFT JOIN address a ON u.address_id = a.address_id
+    LEFT JOIN contact_details c ON u.contact_id = c.contact_id
+    LEFT JOIN clinics s ON u.clinic_id = s.clinic_id
+    LEFT JOIN images i ON u.image_id = i.image_id
+    WHERE u.user_id = ? AND u.user_type = 'pet_doctor'`;
+    try{
+        const [results] = await db.query(sql, [userId]);
+        const clinicData = JSON.parse(JSON.stringify(results));
 
-//         if (clinicData.length > 0) {
-//             res.status(200).json({
-//                 clinicData,
-//                 message:messages.SUCCESS_MESSAGE,
-//             });
-//         } else {
-//             res.status(200).json({
-//                 message: messages.NO_DATA,
-//             });
-//         }
+        if (clinicData.length > 0) {
+            res.status(200).json({
+                clinicData,
+                message:messages.SUCCESS_MESSAGE,
+            });
+        } else {
+            res.status(200).json({
+                message: messages.NO_DATA,
+            });
+        }
 
-//     }catch(err){
-//         logger.error('Error fetching data:', err);
-//         res.status(400).json({
-//             message: messages.FAILURE_MESSAGE,
-//         });
-//     }
-// });
-
-
-
-// clinic.put('/update-clinic', jwtMiddleware.verifyToken, async (req, res) => {
-//     const clinic_id = req.query.clinic_id;
-
-//     if (!clinic_id) {
-//         return res.status(400).json({
-//             message: messages.INVALID_ID,
-//         });
-//     }
-
-//     try {
-//         const { clinic_name, specialisation, clinic_license, experience, education, week_start_day, week_end_day, start_time, end_time } = req.body;
-
-//         let clinicSql = 'UPDATE onelove.clinics SET';
-
-//         const values = [];
-
-//         if (clinic_name !== undefined) {
-//             clinicSql += ' clinic_name=?,';
-//             values.push(clinic_name);
-//         }
-//         if (specialisation !== undefined) {
-//             clinicSql += ' specialisation=?,';
-//             values.push(JSON.stringify(specialisation));
-//         }
-//         if (clinic_license !== undefined) {
-//             clinicSql += ' clinic_license=?,';
-//             values.push(clinic_license);
-//         }
-//         if (experience !== undefined) {
-//             clinicSql += ' experience=?,';
-//             values.push(experience);
-//         }
-//         if (education !== undefined) {
-//             clinicSql += ' education=?,';
-//             values.push(education);
-//         }
-
-//         if (week_start_day !== undefined) {
-//             clinicSql += ' week_start_day=?,';
-//             values.push(week_start_day);
-//         }
-//         if (week_end_day !== undefined) {
-//             clinicSql += ' week_end_day=?,';
-//             values.push(week_end_day);
-//         }
-//         if (start_time !== undefined) {
-//             clinicSql += ' start_time=?,';
-//             values.push(start_time);
-//         }
-//         if (end_time !== undefined) {
-//             clinicSql += ' end_time=?,';
-//             values.push(end_time);
-//         }
-
-//         clinicSql = clinicSql.slice(0, -1);
-//         clinicSql += ' WHERE clinic_id=?';
-//         values.push(clinic_id);
-
-//         await db.beginTransaction();
-
-//         await db.query(clinicSql, values);
-
-//         await db.commit();
-
-//         res.status(200).json({
-//             message: messages.DATA_UPDATED,
-//         });
-//     } catch (err) {
-//         await db.rollback();
-//         logger.error('Error updating data:', err.message);
-//         res.status(400).json({ message: messages.DATA_UPDATE_FALIED });
-//     }
-// });
+    }catch(err){
+        logger.error('Error fetching data:', err);
+        res.status(400).json({
+            message: messages.FAILURE_MESSAGE,
+        });
+    }
+});
 
 
 
-// module.exports=clinic;
+clinic.put('/update-clinic', jwtMiddleware.verifyToken, async (req, res) => {
+    const clinic_id = req.query.clinic_id;
+
+    if (!clinic_id) {
+        return res.status(400).json({
+            message: messages.INVALID_ID,
+        });
+    }
+
+    try {
+        const { clinic_name, specialisation, clinic_license, experience, education, week_start_day, week_end_day, start_time, end_time } = req.body;
+
+        let clinicSql = 'UPDATE onelove.clinics SET';
+
+        const values = [];
+
+        if (clinic_name !== undefined) {
+            clinicSql += ' clinic_name=?,';
+            values.push(clinic_name);
+        }
+        if (specialisation !== undefined) {
+            clinicSql += ' specialisation=?,';
+            values.push(JSON.stringify(specialisation));
+        }
+        if (clinic_license !== undefined) {
+            clinicSql += ' clinic_license=?,';
+            values.push(clinic_license);
+        }
+        if (experience !== undefined) {
+            clinicSql += ' experience=?,';
+            values.push(experience);
+        }
+        if (education !== undefined) {
+            clinicSql += ' education=?,';
+            values.push(education);
+        }
+
+        if (week_start_day !== undefined) {
+            clinicSql += ' week_start_day=?,';
+            values.push(week_start_day);
+        }
+        if (week_end_day !== undefined) {
+            clinicSql += ' week_end_day=?,';
+            values.push(week_end_day);
+        }
+        if (start_time !== undefined) {
+            clinicSql += ' start_time=?,';
+            values.push(start_time);
+        }
+        if (end_time !== undefined) {
+            clinicSql += ' end_time=?,';
+            values.push(end_time);
+        }
+
+        clinicSql = clinicSql.slice(0, -1);
+        clinicSql += ' WHERE clinic_id=?';
+        values.push(clinic_id);
+
+        await db.beginTransaction();
+
+        await db.query(clinicSql, values);
+
+        await db.commit();
+
+        res.status(200).json({
+            message: messages.DATA_UPDATED,
+        });
+    } catch (err) {
+        await db.rollback();
+        logger.error('Error updating data:', err.message);
+        res.status(400).json({ message: messages.DATA_UPDATE_FALIED });
+    }
+});
+
+
+
+module.exports=clinic;
